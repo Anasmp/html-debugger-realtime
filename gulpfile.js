904259928c13fc7d8cb820faa2b41a0bcb366361
Anas mp <youngtechy.com@gmail.com>
2018-10-22 22:12:16 +0530

    initial

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp');
+var bs = require('browser-sync').create(); // create a browser sync instance.
+
+gulp.task('serve', function() {
+    bs.init({
+        server: {
+            baseDir: "./src"
+        }
+    });
+
+    gulp.watch("src/*.html").on("change", bs.reload);
+    gulp.watch("src/*.css").on("change", bs.reload);
+});
